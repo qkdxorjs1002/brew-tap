a00c5b4b911bbe17ed4fcd043c033319f153a63c
paragonnov <qkdxorjs1002@naver.com>
2023-07-28 15:39:25 +0900

    Update anisched.rb

--- Casks/anisched.rb
+++ Casks/anisched.rb
@@ -1,6 +1,6 @@
 cask "anisched" do
-    version "2.4.9"
-    sha256 "65f00ee71d8880b92909bb8b4e5631570128b78e428792dcd1107111c24c028b"
+    version "3.0.2"
+    sha256 "4df743085d050c4b6414f5f29381c55a259c6c701de072ef9b0394067f8b1758"
   
     url "https://github.com/qkdxorjs1002/AniSched-Desktop/releases/download/#{version}/AniSched.#{version}.dmg"
     name "AniSched"
